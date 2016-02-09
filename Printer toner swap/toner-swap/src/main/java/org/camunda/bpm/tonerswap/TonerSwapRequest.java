002ea1124ea84f85933db095ed19310651c86259
Mark Saluta <msaluta@tervis.com>
2016-02-09 16:54:35 -0500

    initial commit

---
+++ Printer toner swap/toner-swap/src/main/java/org/camunda/bpm/tonerswap/TonerSwapRequest.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.tonerswap;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Toner Swap Request")
+public class TonerSwapRequest extends ServletProcessApplication {
+
+}
